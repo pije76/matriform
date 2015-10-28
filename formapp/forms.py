b2840b960bc15d651b04f18a11aa2853bafb7453
rjdp <rjdp9736@gmail.com>
2015-10-28 11:42:56 +0000

    login form bootstraped

--- formapp/forms.py
+++ formapp/forms.py
@@ -1,6 +1,8 @@
 from django.forms import ModelForm
 from .models import matriaspirants
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 
 class regform(ModelForm):
 
@@ -16,3 +18,4 @@ class regform(ModelForm):
 		self.fields['dob'].widget.format = '%d/%m/%Y'
 		# at the same time, set the input format on the date field like you want it:
 		self.fields['dob'].input_formats = ['%d/%m/%Y']
+		self.helper = FormHelper(self)
