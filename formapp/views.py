97003b705e23a35e22363d14bf7c3d5ec4863484
rjdp <rjdp9736@gmail.com>
2015-10-28 10:50:31 +0000

    add glyphicons

--- formapp/views.py
+++ formapp/views.py
@@ -7,18 +7,18 @@ from django.shortcuts import redirect
 # https://coderwall.com/p/bz0sng/simple-django-image-upload-to-model-imagefield
 
 
-def main(request):
-	return render(request, 'main.html')
-
-
 # def main(request):
-# 	if request.method == 'POST':
-# 		form = regform(request.POST, request.FILES)
-# 		if form.is_valid():
-# 			form.save()
-# 			return redirect('home')
-# 		else:
-# 			return render(request, 'reg.html',{'form':form})
-# 	else:
-# 		form = regform()
-# 	return render(request, 'main.html',{'form':form})
+# 	return render(request, 'main.html')
+
+
+def main(request):
+	if request.method == 'POST':
+		form = regform(request.POST, request.FILES)
+		if form.is_valid():
+			form.save()
+			return redirect('home')
+		else:
+			return render(request, 'reg.html',{'form':form})
+	else:
+		form = regform()
+	return render(request, 'main.html',{'form':form})
